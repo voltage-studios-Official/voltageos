252002e69823e71fb9b9d87eeaca41bffeb43266
TannerVoltageOfficial <tannervoltage@gmail.com>
2025-01-16 20:15:29 +0000

    cool

--- jacdac.ts
+++
@@ -1,8 +0,0 @@
-// auto-generated, do not edit.
-namespace myModules {
-
-    //% fixedInstance whenUsed block="realTimeClock3"
-    export const realTimeClock3 = new modules.RealTimeClockClient("realTimeClock3");
-
-}
-    
